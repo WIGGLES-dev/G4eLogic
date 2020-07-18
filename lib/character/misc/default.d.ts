00e4b0a827a093090712c2563b83390c5f3c1e04
Ian <ian.bell.1997@gmail.com>
2020-07-18 18:24:09 -0400

    rebuild

---
+++ lib/character/misc/default.d.ts
@@ -0,0 +1,18 @@
+import { json } from "utils/json_utils";
+declare enum DefaultType {
+    skill = "Skill"
+}
+export declare abstract class DefaultList {
+}
+export declare abstract class Default<T> {
+    tag: string;
+    type: DefaultType;
+    modifier: number;
+    name?: string;
+    specialization?: string;
+    owner: T;
+    constructor(owner: T);
+    toJSON(): void;
+    loadJSON(object: json): this;
+}
+export {};
